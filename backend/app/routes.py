bfe65c17edabfa748a85895983f0e0ae50e3d6e4
ReGeLePuMa <scpetrea@gmail.com>
2024-04-14 14:59:41 +0300

    Fixed / JSON format

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -390,5 +390,5 @@ def state_mean_by_category_request():
 @webserver.route('/')
 @webserver.route('/api')
 def index():
-    routes = [f"Endpoint: \{rule}\ Methods: {','.join(rule.methods)}" for rule in webserver.url_map.iter_rules()]
+    routes = [f"Endpoint: {rule.rule} , Methods: {','.join(rule.methods)}" for rule in webserver.url_map.iter_rules() if not rule.rule.startswith('/static')]
     return jsonify({"status" : "ok", "message": "Welcome to the API!", "routes" : routes}), 200
